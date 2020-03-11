f2c0efc1b650597e12e362b8498ea51e980e2218
sergiolram <sergiolramirez2210@gmail.com>
2020-03-10 21:04:20 -0400

    first commit

---
+++ SingletonPatronDiseno/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SingletonPatronDiseno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.mensaje);
+            Singleton.Instance.mensaje = "Hola Luis";
+            Console.WriteLine(Singleton.Instance.mensaje);
+        }
+    }
+}
